b41dae648bd439d214c510da93f5ede47790de1f
ConvexDegen <0xgamma@protonmail.com>
2021-10-29 13:45:04 +0200

    feat(redacted): dog better than blob bark bark

--- src/views/Landing/components/Background/index.tsx
+++ src/views/Landing/components/Background/index.tsx
@@ -1,20 +1,19 @@
 import React from "react";
 import "./background.scss";
 import { logoImg } from "../../../../constants/img";
-import BlobsTop from "../../../../assets/icons/landing-blobs-top.png";
-import BlobsCenter from "../../../../assets/icons/landing-blobs-center.png";
+import GoodBoy from "../../../../assets/icons/shib.jpeg";
 
 function Background() {
     return (
         <div className="landing-background">
             <div className="landing-background-blobs-top">
-                <img alt="" src={BlobsTop} />
+                <img alt="" src={GoodBoy} />
             </div>
             <div className="landing-background-wrap">
                 <p> {logoImg} </p>
             </div>
             <div className="landing-background-blobs-center">
-                <img alt="" src={BlobsCenter} />
+                <img alt="" src={GoodBoy} />
             </div>
         </div>
     );
